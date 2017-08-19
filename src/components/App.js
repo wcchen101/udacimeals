da81537f1be01313e303e0174ebeb1b3b2555521
Wei-Chung Chen <wcchen101@gmail.com>
2017-08-19 00:01:34 -0700

    fix bugs

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { addRecipe, removeFromCalendar} from '../actions'
-import { CalendarIcon } from 'react-icons/lib/fa/calendar-plus-o'
+import CalendarIcon from 'react-icons/lib/fa/calendar-plus-o'
 import { capitalize } from '../utils/helpers'
 
 class App extends Component {
